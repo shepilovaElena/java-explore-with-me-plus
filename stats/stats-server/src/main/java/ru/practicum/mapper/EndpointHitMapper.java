9c14b59d2919ccb5dade5ec1d8267517ccbab6f9
Shepilova <magic.hollow@yandex.kz>
2025-05-09 16:44:47 +0400

    checkstyle

--- stats/stats-server/src/main/java/ru/practicum/mapper/EndpointHitMapper.java
+++ stats/stats-server/src/main/java/ru/practicum/mapper/EndpointHitMapper.java
@@ -1,13 +1,11 @@
 package ru.practicum.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.model.EndpointHit;
 
 @Mapper(componentModel = "spring")
 public interface EndpointHitMapper {
-
     EndpointHit toEntity(EndpointHitDto endpointHitDto);
 
     EndpointHitDto toDto(EndpointHit endpointHit);
