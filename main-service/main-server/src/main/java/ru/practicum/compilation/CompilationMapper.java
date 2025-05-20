93a1b49377e5fdb7e35c6a689dd1b4fb5adc2a5f
JekaCAP <agentgeka@yandex.ru>
2025-05-20 15:21:30 +0500

    fixEventMapperAndLogic.2

--- main-service/main-server/src/main/java/ru/practicum/compilation/CompilationMapper.java
+++ main-service/main-server/src/main/java/ru/practicum/compilation/CompilationMapper.java
@@ -1,6 +1,5 @@
 package ru.practicum.compilation;
 
-import jakarta.validation.constraints.Max;
 import org.mapstruct.Mapper;
 import ru.practicum.dto.compilation.CompilationDto;
 import ru.practicum.dto.event.EventShortDto;
