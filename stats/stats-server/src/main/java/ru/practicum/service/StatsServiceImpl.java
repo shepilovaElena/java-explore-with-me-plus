75c2b500965f2d606e4a8840c5dcb7346c87e989
DenisovKN <denisov.kn@yandex.ru>
2025-05-24 06:15:33 +0300

    feat: Фикс статистики

--- stats/stats-server/src/main/java/ru/practicum/service/StatsServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/service/StatsServiceImpl.java
@@ -39,12 +39,6 @@ public class StatsServiceImpl implements StatsService {
             uris = null;
         }
 
-        if (uris != null) {
-            uris = uris.stream()
-                    .map(uri -> uri.startsWith("/") ? uri : "/" + uri)
-                    .toList();
-        }
-
         List<ViewStatsDto> result = unique ?
                 repository.findUniqueHits(start, end, uris) :
                 repository.findAllHits(start, end, uris);
