92758c452130f7273d4770ade8e2d39adfc1e91e
DenisovKN <denisov.kn@yandex.ru>
2025-05-26 07:36:21 +0300

    feat: Отладить комментарии

--- main-service/main-dto/src/main/java/ru/practicum/dto/comment/NewCommentDto.java
+++ main-service/main-dto/src/main/java/ru/practicum/dto/comment/NewCommentDto.java
@@ -7,18 +7,9 @@ import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 @Data
-@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class NewCommentDto {
     @NotBlank
     @Size(min = 3, max = 3000)
     String content;
-
-    @NotNull
-    @Positive
-    Long user;
-
-    @NotNull
-    @Positive
-    Long event;
 }
